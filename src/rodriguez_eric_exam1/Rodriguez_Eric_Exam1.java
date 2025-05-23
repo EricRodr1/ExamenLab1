c3c2400fdfd056f9e9ab49130ed6a745445be07f
Eric Rodriguez <Eric Rodriguez@ERIC-RODRIGUEZ>
2025-05-23 09:58:46 -0600

    Avance

--- src/rodriguez_eric_exam1/Rodriguez_Eric_Exam1.java
+++ src/rodriguez_eric_exam1/Rodriguez_Eric_Exam1.java
@@ -16,7 +16,10 @@ public class Rodriguez_Eric_Exam1 {
      */
     public static void main(String[] args) {
          int filas = 0; 
+           
+           String clavecifrada="";
         Scanner sc = new Scanner (System.in);
+        Random random = new Random ();
         sc.useDelimiter("\n");
         int opcion =0;
         // TODO code application logic here
@@ -41,49 +44,81 @@ public class Rodriguez_Eric_Exam1 {
         for (int fila = 1; fila <= filas; fila++) {
             int sumadefilas = 0; 
             String tfila = "";  
-
-            
             for (int i = 0; i < fila; i++) {
                 tfila = tfila + numactual; 
                 sumadefilas = sumadefilas + numactual;   
-
-                
                 if (i < fila - 1) {
                     tfila = tfila + " ";
                 }
                 numactual = numactual + 2;
             }
-
-           
             System.out.println(tfila + " = " + sumadefilas);
-        }
-
+        } 
             break;
                 case 2:
                     String clave = "";
                     System.out.println("Favor ingrese su texto cifrado");
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
+                    clave = sc.next();
+                           
+                    /*for (int i = 0; i < clave.length(); i++) {
+                        char caracter = clave.charAt(i);
+                        if ( caracter >= 'a' && caracter <= 'z'){
+                            caracter = (char) ('a' + 'z' - caracter);
+                        } 
+                        clavecifrada = clave + caracter;
+                    }
+                    System.out.println("Mensaje cifrado: "+clavecifrada);*/
+            
+        }
+       
+        System.out.println("Oracion cifrada: " + clavecifrada);
+
+     
+
                     break;
                 case 3: 
+                  String eleccionusuario="";
+                    boolean valido = false;
+                    while (valido == false){
                     System.out.println("Bienvenido a piedra papel o tijera!");
-                    System.out.println("Favor ingrese: piedra, papel o tijera ");
-                    String eleccionusuario="";
-                    eleccionusuario = sc.next();
+                        System.out.println("Favor eliga: piedra papel o tijera");
+                        eleccionusuario = sc.next().toLowerCase();
+                        if (eleccionusuario.equals("piedra") || eleccionusuario.equals("tijera") || eleccionusuario.equals("papel")){
+                        valido = true;
+                    } else {
+                        System.out.println("Opcion invalida, intente de nuevo");
+                        valido = false;
+                    }
+                    }
                     
+                    int aleatorio = random.nextInt(3);
+                    String computadora = ""; //La eleccion de la computadora 
+                    if (aleatorio == 1){
+                        computadora = "papel";
+                    } else if (aleatorio == 2){
+                        computadora = "tijera";
+                    } else if (aleatorio == 3){
+                            computadora = "piedra";
+                            }
+                    System.out.println("Computadora: "+computadora);
+                   
+                    if (eleccionusuario.equals(computadora)){
+                        System.out.println("Empate ");
+                    } else if (eleccionusuario.equals("papel") && computadora.equals("piedra"))  {
+                        System.out.println("Usted gana");
+                    } else if (eleccionusuario.equals("tijera") && computadora.equals("papel")){
+                            System.out.println("Usted gana");
+                            } else if (eleccionusuario.equals("piedra") && computadora.equals("tijera")){
+                                System.out.println("Usted gana");
+                            } else {
+                                System.out.println("La computadora gana");
+                            }
+                     break;
+                case 4:
                     
+                default:
+                    System.out.println("Opcion no valida, intente de nuevo");
                     break;
-                    
             }
         } while (opcion !=5);
     }
